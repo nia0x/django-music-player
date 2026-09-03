e099d756015ee1d61d06eb761784fe0e44636e31
nia0x <s78951124@gmail.com>
2026-09-03 20:53:51 +0330

    Fix allowed hosts

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-&kovx^t=+t$pfx_=xmpyi$3b6u-9brcrho5m^w)_j#acjgs3k=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "django-music-player-8cbq.onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
