27087ffdb7148345d18c924f7ae79358d218e144
nia0x <s78951124@gmail.com>
2026-09-03 20:59:17 +0330

    Add home music page

--- core/urls.py
+++ core/urls.py
@@ -15,15 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from music import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('music/',include('music.urls')),
+    path('music/', include('music.urls')),
+    path('', views.music, name='home'),
 ]
-
-
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
